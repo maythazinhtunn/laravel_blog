fee81f628558c186f274adfcfbd5ed6a46c384f0
May Thazin Htun <may.thazin.htun@frontiir.net>
2022-08-14 23:56:59 +0630

    test route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/articles', function () {
+    return 'Article List';
+    });
+Route::get('/articles/detail', function () {
+    return 'Article Detail';
+    })-> name('article.detail');
+
+Route::get('/articles/detail/{id}',function($id){
+    return "Article_details $id";
+});
+
+Route::get('/articles/more',function(){
+    return redirect()->route('article.detail');
+});
